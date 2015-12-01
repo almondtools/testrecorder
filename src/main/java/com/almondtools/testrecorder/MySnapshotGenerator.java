fbecc4a0c61a026fa75ba47165e6d99f34b97fa5
Stefan Mandel <mandelst@gmail.com>
2015-12-01 14:28:25 +0100

    removing Dummy Implementation

--- src/main/java/com/almondtools/testrecorder/MySnapshotGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.almondtools.testrecorder;
-
-public class MySnapshotGenerator extends SnapshotGenerator {
-
-	public MySnapshotGenerator(Object self, Class<? extends SnapshotConfig> config) {
-		super(self, config);
-	}
-
-}
