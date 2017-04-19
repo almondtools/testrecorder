07828202c4a52facbcbda46ba78aca9d6b5e330a
almondtools <mandelst@gmail.com>
2017-04-19 08:37:10 +0200

    more generic deserializer context

--- src/main/java/net/amygdalum/testrecorder/deserializers/DeserializerContext.java
+++ src/main/java/net/amygdalum/testrecorder/deserializers/DeserializerContext.java
@@ -2,35 +2,42 @@ package net.amygdalum.testrecorder.deserializers;
 
 import static java.util.Arrays.asList;
 
-import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class DeserializerContext {
     
     public static final DeserializerContext NULL = new DeserializerContext();
     
-    private List<Annotation> annotations;
+    private List<Object> hints;
 
     public DeserializerContext() {
-        this.annotations = new ArrayList<>();
+        this.hints = new ArrayList<>();
     }
     
-    public DeserializerContext(Collection<Annotation> annotations) {
-        this.annotations = new ArrayList<>(annotations);
+    public DeserializerContext(Collection<Object> annotations) {
+        this.hints = new ArrayList<>(annotations);
     }
 
-    public <T extends Annotation> Optional<T> getHint(Class<T> clazz) {
-        return annotations.stream()
-            .filter(annotation -> clazz.isInstance(annotation))
-            .map(annotation -> clazz.cast(annotation))
+    public <T> Optional<T> getHint(Class<T> clazz) {
+        return hints.stream()
+            .filter(hint -> clazz.isInstance(hint))
+            .map(hint -> clazz.cast(hint))
             .findFirst();
     }
 
-    public static DeserializerContext newContext(Annotation... annotations) {
-        return new DeserializerContext(asList(annotations));
+    public <T> Stream<T> getHints(Class<T> clazz) {
+        return hints.stream()
+            .filter(hint -> clazz.isInstance(hint))
+            .map(hint -> clazz.cast(hint));
+    }
+
+    @SafeVarargs
+    public static <T extends Object> DeserializerContext newContext(T... objects) {
+        return new DeserializerContext(asList(objects));
     }
 
 }
