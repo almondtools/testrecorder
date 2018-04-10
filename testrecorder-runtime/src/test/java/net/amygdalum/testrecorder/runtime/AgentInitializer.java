626db8a4a160d7da63db08d4309c82fef71ca59d
almondtools <mandelst@gmail.com>
2018-04-10 05:44:35 +0200

    removing package cycle

--- testrecorder-runtime/src/test/java/net/amygdalum/testrecorder/runtime/AgentInitializer.java
+++ testrecorder-runtime/src/test/java/net/amygdalum/testrecorder/runtime/AgentInitializer.java
@@ -1,6 +1,7 @@
-package net.amygdalum.testrecorder.util;
+package net.amygdalum.testrecorder.runtime;
 
 import net.amygdalum.testrecorder.runtime.TestRecorderAgentInitializer;
+import net.amygdalum.testrecorder.util.Logger;
 
 public class AgentInitializer implements TestRecorderAgentInitializer {
 
